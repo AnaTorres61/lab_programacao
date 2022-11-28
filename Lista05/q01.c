400eac4060912d72dc3851ef83b429bac77dd071
AnaTorres61 <ana.torres61@aluno.ifce.edu.br>
2022-11-28 10:26:54 -0300

    Add Lista05

---
+++ Lista05/q01.c
@@ -0,0 +1,8 @@
+//Escreva um programa que receba dois valores por linha de execucao e realize a soma ou multiplicacao, conforme um codigo de operacao tambem recebido por linha de execucao.
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
